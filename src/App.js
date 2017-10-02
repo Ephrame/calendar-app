5170aa18a3371905ce67168128b4fc6b283f35c1
Ephrame <viollet.ame@gmail.com>
2017-10-02 11:45:34 +0200

    multi c

--- src/App.js
+++ src/App.js
@@ -2,54 +2,83 @@ import React, { Component } from 'react'
 import Calendar from './components/calendar'
 import './App.css'
 import moment from 'moment'
-const myCalendarElement = [
-  {
-    start: '30',
-    end: '90',
-    comment: 'First Exemple'
-  },
-  {
-    start: '30',
-    end: '70',
-    comment: 'First Exemple'
-  }
-]
-
-function transformElements (elms) {
-  const referenceHour = moment().hour(9).minute(0)
-  return elms.reduce((acc, planningElm) => {
-    const hourSinceReferenceHour = moment(referenceHour).add(planningElm.start, 'minutes')
-    const endHourSinceReferenceHour = moment(referenceHour).add(planningElm.end, 'minutes')
-    const hourFind = hourSinceReferenceHour.hour()
-    const endHour = endHourSinceReferenceHour.hour()
-    // const hourInTotalSinceReference = referenceHour.add(planningElm.end, 'minutes')
-    const diffHour = endHour - hourFind
-    const diffArray = Array(diffHour + 1).fill(1)
-    diffArray.map((diffHour, index) => {
-      const newReference = moment().hour(hourFind + index).minute(0)
-      const diffMinutesToStart = hourSinceReferenceHour.diff(newReference, 'minutes')
-      const diffMinutesToEnd = endHourSinceReferenceHour.diff(newReference, 'minutes')
-      acc[newReference.hour()] = [
-        ...(acc[newReference.hour()] ? acc[newReference.hour()] : {}),
-        {
-          ...planningElm,
-          diffMinute: [diffMinutesToStart + 1 > 0 ? diffMinutesToStart : 0, diffMinutesToEnd + 1]
+import sortBy from 'lodash/sortBy'
+import difference from 'lodash/difference'
+const myCalendarElementT = [ {start: 30, end: 150}, {start: 30, end: 70}, {start: 30, end: 140}, {start: 30, end: 70}, {start: 560, end: 620}, {start: 610, end: 670} ];
+const myCalendarElement= [{start: 30, end: 150}, {start: 540, end: 600}, {start: 560, end: 620}, {start: 610, end: 670}]
+function addTheHour (elms) {
+  const sortedElementWithInformation = sortBy(elms, ['start']).reduce((acc, elm, index) => {
+    const lastColision = Object.keys(acc.newElements).reduce((colisions, key, index) => {
+      const toFilter = acc.newElements[key]
+      if(elm.start < toFilter.end){
+        colisions.push({coli: toFilter.colision, index})
+      }
+      return colisions
+    } , [])
+    if(lastColision.length === 1 ) {
+      const result =  {
+        previous: {...elm, colision: !acc.previous.colision ? 'C2' : (acc.previous.colision === 'C1' ? 'C2' :'C1')},
+        newElements: {
+          ...acc.newElements,
+          [index - 1]: {
+            ...acc.newElements[index - 1],
+            numberOfColisition: 2,
+            colision: !acc.previous.colision ? 'C1' : acc.previous.colision
+          },
+          [index]: {
+            ...elm,
+            numberOfColisition: 2,
+            colision: (!acc.previous.colision) ? 'C2' : (
+              acc.previous.colision === 'C1' ? // ToDO ET QUE START
+               'C2' :'C1')
+          }
         }
-      ]
-    })
-    return acc
-  }, {})
+      }
+      return result
+    }else if (lastColision.length >= 2){
+      const tailleLastColision = lastColision.length + 1
+      const numberAvailable = difference(["C1", "C2", "C3", "C4"],lastColision.map(e => e.coli))
+      const result =  {
+        previous: {
+          ...elm,
+          colision: "C"+ (lastColision.length +1)
+        },
+        newElements: {
+          ...acc.newElements,
+          [index]: {
+            ...elm,
+            numberOfColisition: tailleLastColision,
+            colision: "C"+ (lastColision.length +1)
+          }
+        }
+      }
+      lastColision.map(coli => {
+         result.newElements[coli.index] = {
+          ...result.newElements[coli.index],
+          numberOfColisition: tailleLastColision
+        }
+      })
+      return result
+    }
+    return {
+      prePrevious: acc.previous,
+      previous: elm,
+      newElements: {
+        ...acc.newElements,
+        [index]: elm
+      }
+    }
+  }, {previous: {}, newElements: {}})
+  return sortedElementWithInformation
 }
 
 class App extends Component {
   render () {
     return (
       <div className='App'>
-        <header className='App-header'>
-          <h1 className='App-title'>Calendar App</h1>
-        </header>
+
         <div className='App-content'>
-          <Calendar elements={transformElements(myCalendarElement)} />
+          <Calendar elements={addTheHour(myCalendarElement).newElements} />
         </div>
       </div>
     )
--- src/components/calendar.js
+++ src/components/calendar.js
@@ -7,12 +7,55 @@ import Patern from './patern'
 class Calendar extends Component {
   render () {
     const hourArray = Array(13).fill(1).map((elm, index) => {
-      const hour = 9 + index
-      return hour
+      const hour = (index * 60)
+      return {diff: hour, hour: 9 + index}
     })
     return (
+      <div style={{display:'flex', position: 'relative'}}>
+      <div>{
+        hourArray.map(elm => {
+          return <div>
+            <span className='hour' style={{ position: 'absolute',top: ` ${-8 +elm.diff}px`, left: '-65px'}}>{`${moment().hour(elm.hour).minute(0).format('hh:mm A')} -`}</span>
+            <span  className='hour' style={{ position: 'absolute',top: `${-6 +elm.diff + 30}px`,  left: '-25px', fontSize: '10px'}}>{`${moment().hour(elm.hour).minute(30).format('hh:mm')} -`}</span>
+
+          </div>
+        })
+      }</div>
       <div className='calendar'>
         {
+          Object.keys(this.props.elements).map((key,index) => {
+            const elm = this.props.elements[key]
+            const purcentTT =  elm.end - elm.start
+            const colision = elm.colision ? +elm.colision.split('C')[1] : ''
+            const widthElm = elm.colision ? 1 / elm.numberOfColisition * 100 - 2 : 94
+            const leftElm = elm.colision ? 100 - (colision / elm.numberOfColisition * 100) : 0
+            return <div
+            className='element'
+            style={{
+              height: `${purcentTT}px`,
+              left: `${leftElm}%`,
+              marginLeft: `${colision === 1 ? '0px': '2%' }`,
+              top: `${elm.start}px`,
+              width: `${widthElm}%`
+            }}
+            >
+              <div className='blue'></div>
+            </div>
+          })
+        }
+      </div>
+      </div>
+    )
+  }
+}
+
+Calendar.defaultProps = {
+  elements: {},
+}
+
+
+export default Calendar
+/**        {
           hourArray.map(elm => {
             return (
               <div>
@@ -20,13 +63,4 @@ class Calendar extends Component {
               </div>
             )
           })
-        }
-      </div>
-    )
-  }
-}
-
-Calendar.defaultProps = {
-  elements: {}
-}
-export default Calendar
+        }**/
